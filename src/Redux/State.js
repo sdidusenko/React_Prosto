5114664436eb79b75bad48128a42db977546d1c9
Serhii Didusenko <didusenko40@gmail.com>
2023-02-08 21:17:51 +0200

    Add STATE object, and all functions from state file into new STORE object. And readding all ways of props in index.js file(add 'bind')

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -11,11 +11,78 @@ import Olga from './../Assets/Olga.jpg'
 import Petro from './../Assets/Petro.jpg'
 import Nadiya from './../Assets/Nadiya.jpg'
 /*import {renderAllTree} from "../index";*/
-let renderAllTree =() =>{
+/*let renderAllTree =() =>{
     console.log('state changed')
+}*/
+let store={
+    _state: {
+        profilePage: {
+            newPostText: 'It-kam',
+            post: [
+                {message: "Hello, I am good", like: 7, img: Cat},
+                {message: "It is my first post", like: 77, superlike: 777, img: Frog},
+                {message: "HI, YO, Girls", like: 7777, superlike: 77777, img: Man},
+            ],
+            friends: [
+                {name: 'Olga', id: 1, img: Olga},
+                {name: 'Petro', id: 2, img: Petro},
+                {name: 'Nadiya', id: 3, img: Nadiya},
+            ],
+
+        },
+        messagePage: {
+            newMessage:'tap new message',
+            messagesData: [
+                {message: 'Hi', id: 1},
+                {message: 'How is your it-kamasutra?', id: 2},
+                {message: 'Yo', id: 3},
+                {message: 'Yo', id: 4},
+                {message: 'Yo', id: 5},
+            ],
+            dialogsData: [
+                {name: 'Dima', id: 1, img: Dima},
+                {name: 'Andrey', id: 2, img: Andrey},
+                {name: 'Sveta', id: 3, img: Sveta},
+                {name: 'Sasha', id: 4, img: Sasha},
+                {name: 'Viktor', id: 5, img: Viktor},
+                {name: 'Valera', id: 6, img: Valera},
+            ],
+        }
+
+
+    },
+    getState(){
+        return this._state
+    },
+    _renderAllTree(){
+        console.log('state changed')
+},
+    adPost(postMessage){
+        let newPost={message: postMessage, like: 3, superlike: 2, img: Valera}
+        this._state.profilePage.post.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._renderAllTree(this._state)
+    },
+    adNewMessage(newMessage){
+        let messages ={message:newMessage, id:6}
+        this._state.messagePage.messagesData.push(messages)
+        this._state.messagePage.newMessage= ''
+        this._renderAllTree(this._state)
+    },
+    updateNewMessage (mess){
+        this._state.messagePage.newMessage=mess
+        this._renderAllTree(this._state)
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText= newText
+        this._renderAllTree(this._state)
+    },
+    subscribe(observer){
+        this._renderAllTree=observer
+    }
 }
 
-let state = {
+/*let state = {
     profilePage: {
         newPostText: 'It-kam',
         post: [
@@ -50,32 +117,31 @@ let state = {
     }
 
 
-}
-window.state=state
-export const adNewMessage=(newMessage)=>{
+}*/
+window.store=store
+/*export const adNewMessage=(newMessage)=>{
     let messages ={message:newMessage, id:6}
     state.messagePage.messagesData.push(messages)
     state.messagePage.newMessage= ''
     renderAllTree(state)
-}
-export const updateNewMessage =(mess)=>{
+}*/
+/*export const updateNewMessage =(mess)=>{
     state.messagePage.newMessage=mess
     renderAllTree(state)
-}
-export const adPost = (postMessage) => {
+}*/
+/*export const adPost = (postMessage) => {
     let newPost={message: postMessage, like: 3, superlike: 2, img: Valera}
     state.profilePage.post.push(newPost)
     state.profilePage.newPostText = ''
     renderAllTree(state)
-
-}
-export const updateNewPostText = (newText) => {
+}*/
+/*export const updateNewPostText = (newText) => {
 state.profilePage.newPostText= newText
   renderAllTree(state)
-}
+}*/
 
-export const subscribe=(observer)=>{
+/*export const subscribe=(observer)=>{
     renderAllTree=observer
-}
+}*/
 
-export default state
+export default store
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import reportWebVitals from './reportWebVitals';
-import {adNewMessage, adPost, subscribe, updateNewMessage, updateNewPostText} from "./Redux/State";
-import state from "./Redux/State";
-
+import store from './Redux/State'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,24 +8,24 @@ import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
- let renderAllTree = (state) => {
+ let renderAllTree = () => {
 
     root.render(
         /*<React.StrictMode>*/
         <BrowserRouter><App
-            appState={state}
-            adPost={adPost}
-            updateNewPostText={updateNewPostText}
-            adNewMessage={adNewMessage}
-            updateNewMessage={updateNewMessage}
+            appState={store.getState()}
+            adPost={store.adPost.bind(store)}
+            updateNewPostText={store.updateNewPostText.bind(store)}
+            adNewMessage={store.adNewMessage.bind(store)}
+            updateNewMessage={store.updateNewMessage.bind(store)}
         /></BrowserRouter>/*переніс з арр.js
         /*</React.StrictMode>*//* this string we have not in a lesson*/
     );
 }
 
-renderAllTree(state)
+renderAllTree(store.getState())
 
-subscribe(renderAllTree)
+store.subscribe(renderAllTree)
 
 
 
